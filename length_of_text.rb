6491c17c46660fc05fd8755b0a5c5076a5ca3be1
Justafigurehead <strachanmichaela@hotmail.com>
2017-01-10 21:27:55 +0000

    Updated length_of_text.rb and next.rb

--- length_of_text.rb
+++ length_of_text.rb
@@ -1,25 +1,7 @@
-#Basic version
 
-=begin def length_of_text(firstname, surname)
-  return "My first name is #{firstname.length} letters long and my surname is #{surname.length} letters long."
+def length_of_string(string_1)
+  return "Your string is #{string_1.length} letters long."
 end
 
-puts "How many letters is your full name?"
-puts length_of_text("michaela", "surname")
-=end 
-
-#user input 
-
-def length_of_word(firstname, surname)
-  return "There are #{firstname.length + surname.length} letters in your full name!"
-end
-
-puts "We're going to calculate how many letters are in your first and surname name! :)"
-
-puts "What is your first name?"
-firstname = gets.chomp
-
-puts "What is your surname?"
-surname = gets.chomp
-
-puts length_of_word(firstname, surname)
+string_1 = "Gee, this is short."
+puts length_of_string(string_1)
--- next.rb
+++ next.rb
@@ -1,4 +1,11 @@
+#
+
 def next_number(number)
   return  number + 1
 end
-puts next_number(3)
+
+number = 7
+number_1 = 8
+puts "the next number after #{number} is #{next_number(number)}"
+puts "the next number after #{number_1} is #{next_number(number_1)}."
+
